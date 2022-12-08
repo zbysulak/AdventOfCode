fa30044335ea9cef51fa03a4af32264d7c8b0393
zbysek.sedlacek <zbysek.sedlacek@digiteqautomotive.com>
2022-12-08 08:30:28 +0100

    task 6

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode;
+
+string[] lines = System.IO.File.ReadAllLines(@"..\..\..\input\06");
+
+var task = new Task06();
+task.Solve(lines);
+task.Solve2(lines);
---
+++ Task06.cs
@@ -0,0 +1,42 @@
+namespace AdventOfCode;
+
+public class Task06
+{
+    public void Solve(string[] lines)
+    {
+        foreach (var line in lines)
+        {
+            var currentPos = 0;
+            while (currentPos < line.Length+4 && !AreAllDifferent(line.Substring(currentPos, 4)))
+            {
+                currentPos++;
+            }
+            Console.WriteLine(currentPos+4);
+        }
+    }
+    
+    public void Solve2(string[] lines)
+    {
+        foreach (var line in lines)
+        {
+            var currentPos = 0;
+            while (currentPos < line.Length+14 && !AreAllDifferent(line.Substring(currentPos, 14)))
+            {
+                currentPos++;
+            }
+            Console.WriteLine(currentPos+14);
+        }
+    }
+
+    private bool AreAllDifferent(string str)
+    {
+        //Console.WriteLine($"checking {str}");
+        var diff = true;
+        for (int i = 0; i < str.Length-1; i++)
+        {
+            //Console.WriteLine($"{str.Substring(i+1)} contains {str[i]}?");
+            diff &= !str.Substring(i+1).Contains(str[i]);
+        }
+        return diff;
+    }
+}
