09c55d5ff8c96f6fa47a232600eaf3373b6f89ca
zbysulak <sedlik31@gmail.com>
2023-01-29 22:30:52 +0100

    added exception for no input file (for task 11)

--- Program.cs
+++ Program.cs
@@ -3,7 +3,16 @@
 ITask task = new Task06();
 var tasknumber = task.GetType().ToString().Substring(4);
 var sample = true;
-string[] lines = System.IO.File.ReadAllLines(@"..\..\..\input\" + tasknumber + (sample ? "-sample" : ""));
+string[] lines;
+try
+{
+    lines = File.ReadAllLines(@"..\..\..\input\" + tasknumber + (sample ? "-sample" : ""));
+}
+catch (FileNotFoundException)
+{
+    lines = Array.Empty<string>();
+}
+
 Console.WriteLine("First part:");
 task.Solve(lines);
 Console.WriteLine("Second part:");
