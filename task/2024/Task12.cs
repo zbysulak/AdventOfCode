38525922ec45f96925f36a8f6ad020c22c87ef69
zbysulak <sedlik31@gmail.com>
2024-12-12 11:30:19 +0100

    day 12 part 2 solved

--- task/2024/Task12.cs
+++ task/2024/Task12.cs
@@ -1,4 +1,4 @@
-// Day 12
+// Day 12: Garden Groups
 
 namespace AdventOfCode.task._2024;
 
@@ -61,7 +61,7 @@ public class Task12 : ITask
         return (area, perimeter);
     }
 
-    //done in  minutes
+    //done in ?? hours
     public void Solve2(string[] lines)
     {
         visited.Clear();
@@ -72,8 +72,8 @@ public class Task12 : ITask
             {
                 if (!visited.Contains((x, y)))
                 {
-                    var (a, _) = Explore(lines, x, y);
-                    var s = CountSides(lines, x, y);
+                    var (a, s) = Explore2(lines, x, y);
+                    //Console.WriteLine($"{lines[y][x]} : area {a}, sides {s}");
                     price += a * s;
                 }
             }
@@ -82,41 +82,105 @@ public class Task12 : ITask
         Console.WriteLine(price);
     }
 
-    private int CountSides(string[] lines, int x, int y)
+    // fence on the right side is not same as fence on the left side of the spot on the right!
+    private (int, int, int)? GetFenceInRow(IList<(int x, int y, int dir)> fences, ( int x, int y, int dir) f, int dist)
     {
-        var sides = 1;
-        var plant = lines[y][x];
-
-        var dir = 1; // 0 - up, 1 - right, 2 - down, 3 - left
-        var (cx, cy) = (x, y);
-        do
+        try
         {
-            var nx = cx + Utils.Directions4[dir][0];
-            var ny = cy + Utils.Directions4[dir][1];
-
-            if (Utils.CheckBounds(lines, nx, ny) && lines[ny][nx] == plant)
+            if (f.dir == 0)
             {
-                // next in line is same, check up
-                var ccwx = nx + Utils.Directions4[(dir + 5) % 4][0];
-                var ccwy = ny + Utils.Directions4[(dir + 5) % 4][1];
-                if (Utils.CheckBounds(lines, ccwx, ccwy) && lines[ccwy][ccwx] == plant)
+                return fences.Single(of => f.y == of.y && f.x - of.x == dist && of.dir == 0);
+            }
+
+            if (f.dir == 2)
+            {
+                return fences.Single(of => f.y == of.y && f.x - of.x == dist && of.dir == 2);
+            }
+
+            if (f.dir == 1)
+            {
+                return fences.Single(of => f.x == of.x && f.y - of.y == dist && of.dir == 1);
+            }
+
+            if (f.dir == 3)
+            {
+                return fences.Single(of => f.x == of.x && f.y - of.y == dist && of.dir == 3);
+            }
+        }
+        catch
+        {
+            return null;
+        }
+
+        return null;
+    }
+
+    private (int area, int sides) Explore2(string[] lines, int x, int y)
+    {
+        var plant = lines[y][x];
+        var area = 0;
+        var fences = new List<(int x, int y, int dir)>();
+
+        var open = new Queue<(int, int)>();
+        open.Enqueue((x, y));
+        while (open.Count > 0)
+        {
+            var (cx, cy) = open.Dequeue();
+            if (!visited.Add((cx, cy)))
+            {
+                continue;
+            }
+
+            area++;
+            for (int i = 0; i < Utils.Directions4.Length; i++)
+            {
+                var d = Utils.Directions4[i];
+                var nx = cx + d[0];
+                var ny = cy + d[1];
+
+                if (!Utils.CheckBounds(lines, nx, ny) || lines[ny][nx] != plant)
+                    fences.Add((cx, cy, i));
+                else
                 {
-                    dir = (dir + 3) % 4;
-                    sides++;
+                    open.Enqueue((nx, ny));
                 }
-
-                cx = nx;
-                cy = ny;
             }
-            else
+        }
+
+        var filteredFences = new List<(int x, int y, int dir)>();
+
+        while (fences.Any())
+        {
+            var f = fences.First();
+            fences.RemoveAt(0);
+            filteredFences.Add(f);
+
+            // I can't just skip fence that has neighbor because it could disconnect other fences in line (i.e. 3 fences in line, 2nd is skipped -> 1st and 3rd are disconnected)
+            var i = 1;
+            while (true)
             {
-                dir = (dir + 1) % 4;
-                sides++;
-            }
-        } while (!(cx == x && cy == y && dir == 0));
+                var next = GetFenceInRow(fences, f, i);
 
-        //todo add inside sides..
-        
-        return sides;
+                if (next == null)
+                    break;
+
+                fences.Remove(next.Value);
+                i++;
+            }
+
+            i = 1;
+            while (true)
+            {
+                var next = GetFenceInRow(fences, f, -i);
+
+                if (next == null)
+                    break;
+
+                fences.Remove(next.Value);
+                i++;
+            }
+        }
+
+        return (area, filteredFences.Count);
     }
 }
